46f87de4fcca52e08b0b8f33e4add0e972392a9d
davaymne <davaymne.misc@gmail.com>
2023-10-01 00:58:06 +0100

    store

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,43 @@ import {processor} from './processor'
 import * as DelegateRegistry from "./abi/DelegateRegistry";
 
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    const delegations: Delegation[] = []
     for (let c of ctx.blocks) {
         for (let log of c.logs) {
+            const delegations: Delegation[] = []
             // decode and normalize the tx data
             if(log.topics[0] === DelegateRegistry.events.SetDelegate.topic) {
                 let {delegator, id, delegate} = DelegateRegistry.events.SetDelegate.decode(log)
                 let delegation = new Delegation({
-                    id: id.toString(),
+                    id: delegator.concat('-').concat(id).concat('-').concat(delegate),
                     delegator: delegator,
                     space: id,
                     delegate: delegate,
-                    timestamp: 
-
-
+                    timestamp: c.header.timestamp,
                 });
+                ctx.log.info(`SetDelegate: block: ${c.header.height}, ${delegation.id}, ${delegation.delegator}, ${delegation.space}, ${delegation.delegate}, ${delegation.timestamp}`);
+                delegations.push(delegation);
+                await ctx.store.upsert(delegations);
+            }
+            if(log.topics[0] === DelegateRegistry.events.ClearDelegate.topic) {
+                let {delegator, id, delegate} = DelegateRegistry.events.ClearDelegate.decode(log)
+                let delegation = new Delegation({
+                    id: delegator.concat('-').concat(id).concat('-').concat(delegate),
+                    delegator: delegator,
+                    space: id,
+                    delegate: delegate,
+                    timestamp: c.header.timestamp,
+                });
+                ctx.log.info(`ClearDelegate: block: ${c.header.height}, ${delegation.id}, ${delegation.delegator}, ${delegation.space}, ${delegation.delegate}, ${delegation.timestamp}`);
+                delegations.push(delegation);
+                await ctx.store.remove(Delegation, [delegation.id]);
             }
         }
     }
     // apply vectorized transformations and aggregations
-    const burned = burns.reduce((acc, b) => acc + b.value, 0n) / 1_000_000_000n
     const startBlock = ctx.blocks.at(0)?.header.height
     const endBlock = ctx.blocks.at(-1)?.header.height
-    ctx.log.info(`Burned ${burned} Gwei from ${startBlock} to ${endBlock}`)
+    ctx.log.info(`Delegations  ${startBlock} to ${endBlock}`)
 
     // upsert batches of entities with batch-optimized ctx.store.save
-    await ctx.store.upsert(burns)
+    // await ctx.store.upsert(delegations);
 })
