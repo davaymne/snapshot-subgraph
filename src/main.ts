d90021ea994ecedc0b5b328f7d54202e4ac02c1d
davaymne <davaymne.misc@gmail.com>
2023-10-02 16:07:56 +0100

    delegateLog

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,12 @@ processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
     const PROXYFACTORY100 = '0x12302fE9c02ff50939BaAaaf415fc226C078613C'.toLowerCase()
     const PROXYFACTORY111 = '0x12302fE9c02ff50939BaAaaf415fc226C078613C'.toLowerCase()
     const PROXYFACTORY130 = '0x12302fE9c02ff50939BaAaaf415fc226C078613C'.toLowerCase()
-    const GNOSISSAFE = '0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446'.toLowerCase()
+    const DELEGATEREGISTRY = '0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446'.toLowerCase()
     const sigs: Sig[] = []
     const delegationsSet: Map<string, Delegation> = new Map()
     const delegationsClear: string[] = []
     for (let c of ctx.blocks) {
-        await ctx.store.upsert(new Block({id: c.header.hash, number: BigInt(c.header.height), timestamp: new Date(c.header.timestamp)}));
+        let delegateLog = false
         for (let log of c.logs) {
             // decode and normalize the tx data GnosisSafe
             if(log.topics[0] === GnosisSafe.events.SignMsg.topic) {
@@ -33,7 +33,7 @@ processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
             }
             // decode and normalize the tx data SetDelegate
             if(log.topics[0] === DelegateRegistry.events.SetDelegate.topic) {
-                if (log.address.toLowerCase()!=GNOSISSAFE) {
+                if (log.address.toLowerCase()!=DELEGATEREGISTRY) {
                     continue
                 }
                 let {delegator, id, delegate} = DelegateRegistry.events.SetDelegate.decode(log);
@@ -47,10 +47,11 @@ processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
                     delegate: delegate,
                     timestamp: new Date(c.header.timestamp),
                 }))
+                delegateLog = true
             }
             // decode and normalize the tx data ClearDelegate
             if(log.topics[0] === DelegateRegistry.events.ClearDelegate.topic) {
-                if (log.address.toLowerCase()!=GNOSISSAFE) {
+                if (log.address.toLowerCase()!=DELEGATEREGISTRY) {
                     continue
                 }
                 let {delegator, id, delegate} = DelegateRegistry.events.ClearDelegate.decode(log);
@@ -58,7 +59,11 @@ processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
                 id  = delegator.concat('-').concat(id).concat('-').concat(delegate).concat('').concat(c.header.timestamp.toString());
                 ctx.log.info(`ClearDelegate: block: ${c.header.height}, ${id}, ${delegator}, ${space}, ${delegate}`);
                 delegationsClear.push(id);
+                delegateLog = true
             }
+        }
+        if (delegateLog === true) {
+            await ctx.store.upsert(new Block({id: c.header.hash, number: BigInt(c.header.height), timestamp: new Date(c.header.timestamp)}));
         }
     }
     // apply vectorized transformations and aggregations
